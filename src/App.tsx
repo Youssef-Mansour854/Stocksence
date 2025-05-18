57edbd83ec405167f37695ab4225967e77ceb17f
Youssef-Mansour <114363242+Youssef-Mansour854@users.noreply.github.com>
2025-05-18 14:05:50 +0300

    Add AuthProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import LandingPage from './pages/LandingPage';
 import AuthPage from './pages/AuthPage';
+import { AuthProvider } from './context/AuthContext';
 import './i18n';
 
 function App() {
@@ -11,12 +12,14 @@ function App() {
 
   return (
     <div dir={dir} className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
